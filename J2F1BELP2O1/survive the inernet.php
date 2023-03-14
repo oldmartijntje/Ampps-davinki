e544cbff6048e769e86a0bc37ad72b823b6bd31c
oldmartijntje <oldmartijntje@gmail.com>
2023-03-14 12:11:27 +0100

    the start of ain page

---
+++ J2F1BELP2O1/survive the inernet.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>formulier</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <script src="main.js" defer></script>
+</head>
+
+<body><?php if ($_POST != "" && $_POST != null){ ?>
+    Welcome <?php echo htmlspecialchars( $_POST["name"] ); ?><br>
+    Your email address is: <?php echo htmlspecialchars( $_POST["email"] ); ?>
+    <?php } else { ?>
+        <form action="welcome.php" method="post">
+        <label for="fname">Name:</label><br>
+        <input type="text" id="name" name="name" required><br>
+        <label for="fname" id="label1"></label><br>
+        <input type="text" id="question1" name="question1" required><br>
+        <label for="fname" id="label2"></label><br>
+        <input type="text" id="question2" name="question1" required><br>
+        <label for="fname" id="label3"></label><br>
+        <input type="text" id="question3" name="question1" required><br>
+        <label for="fname" id="label4"></label><br>
+        <input type="text" id="question4" name="question1" required><br>
+        <label for="fname" id="label5"></label><br>
+        <input type="text" id="question5" name="question1" required><br>
+        <input type="submit">
+    </form> 
+    <?php } ?>
+</body>
